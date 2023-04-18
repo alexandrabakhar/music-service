9454d4b83b6b9d7e77d4c30fb080405d195e7e13
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-04-18 18:39:12 +0300

    added fonts, imgs and icons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MainNav } from './renderMainNav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainNav />
   </React.StrictMode>
 );
 

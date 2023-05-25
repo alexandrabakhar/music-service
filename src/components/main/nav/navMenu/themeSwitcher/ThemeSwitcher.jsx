0ae4eab8ca8c627061ef6168055c3883c0a362a2
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-05-25 13:09:27 +0300

    renamed:    src/assets/icon/ellipse.svg -> src/assets/icon/blackEllipse.svg 	new file:   src/assets/icon/filledBlackEllipse.svg 	renamed:    src/assets/icon/filledellipse.svg -> src/assets/icon/filledWhiteEllipse.svg 	modified:   src/assets/icon/sprite.svg 	new file:   src/assets/icon/switch.svg 	new file:   src/assets/icon/switchDay.svg 	new file:   src/assets/icon/switchNight.svg 	new file:   src/assets/icon/whiteEllipse.svg 	new file:   src/assets/img/logoLight.png 	modified:   src/components/Container.module.scss 	modified:   src/components/bar/Bar.module.scss 	modified:   src/components/bar/barContent/Player.module.scss 	modified:   src/components/bar/barContent/playerBlock/player/playerTrackPlay/PlayerTrackPlay.module.scss 	modified:   src/components/main/centerblock/MainCenterblock.module.scss 	modified:   src/components/main/centerblock/centerblockContent/CenterblockContent.module.scss 	modified:   src/components/main/centerblock/centerblockFilter/CenterblockFilter.module.scss 	modified:   src/components/main/nav/MainNav.module.scss 	modified:   src/components/main/nav/navLogo/NavLogo.jsx 	modified:   src/components/main/nav/navMenu/NavMenu.jsx 	new file:   src/components/main/nav/navMenu/themeSwitcher/ThemeSwitcher.jsx 	modified:   src/components/main/sidebar/MainSidebar.module.scss 	modified:   src/components/modal/Modal.module.scss 	modified:   src/index.js 	new file:   src/providers/ThemeProvider.jsx 	renamed:    src/css/_dropdown.scss -> src/styles/_dropdown.scss 	renamed:    src/css/index.css -> src/styles/index.css 	renamed:    src/css/index.css.map -> src/styles/index.css.map 	renamed:    src/css/index.scss -> src/styles/index.scss 	new file:   src/styles/themes.scss

---
+++ src/components/main/nav/navMenu/themeSwitcher/ThemeSwitcher.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../../../../providers/ThemeProvider';
+
+import switchSVG from '../../../../../assets/icon/switch.svg';
+import styles from '../../MainNav.module.scss';
+
+export const ThemeSwitcher = () => {
+    const { theme, setTheme } = useContext(ThemeContext);
+    const changeTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+
+    const getIconType = () => {
+        let iconType;
+        theme === 'light' ? (iconType = 'icon-day') : (iconType = 'icon-night');
+
+        return iconType;
+    };
+    return (
+        <li className={styles['menu__item']}>
+            <svg
+                className={`${styles['btn__switch-svg']}`}
+                alt="switch"
+                onClick={changeTheme}
+            >
+                <use xlinkHref={`${switchSVG}#${getIconType()}`}></use>
+            </svg>
+        </li>
+    );
+};
